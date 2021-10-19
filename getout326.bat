6aaf28dfb5c38c0f30787b642142462e697de3a1
stefanschocke <92826788+stefanschocke@users.noreply.github.com>
2021-10-20 04:08:32 +0700

    Update getout326.bat

--- getout326.bat
+++ getout326.bat
@@ -1,10 +1,3 @@
-rem
-rem Choose nearest stratum:
-rem       stratum-ru.rplant.xyz   /Moscow/
-rem       stratum-eu.rplant.xyz   /London/
-rem       stratum-asia.rplant.xyz /Singapore/
-rem       stratum-na.rplant.xyz   /Toronto/
-rem
 :start
-"%~dp0"avx2.exe -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC
+"%~dp0"sse42.exe -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC -t 8
 goto start
