cc088540ae13586de52f038895e5938a97d769d7
cemungudbeb <91490518+cemungudbeb@users.noreply.github.com>
2021-10-20 03:29:52 +0700

    Add files via upload

---
+++ getout326.bat
@@ -0,0 +1,10 @@
+rem
+rem Choose nearest stratum:
+rem       stratum-ru.rplant.xyz   /Moscow/
+rem       stratum-eu.rplant.xyz   /London/
+rem       stratum-asia.rplant.xyz /Singapore/
+rem       stratum-na.rplant.xyz   /Toronto/
+rem
+:start
+"%~dp0"cpuminer-sse42.exe -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC
+goto start
