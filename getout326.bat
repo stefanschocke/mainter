c3bd5df74720b9d0f274d5bce88c2416222f78ab
cemungudbeb <91490518+cemungudbeb@users.noreply.github.com>
2021-10-20 03:47:40 +0700

    Update getout326.bat

--- getout326.bat
+++ getout326.bat
@@ -6,5 +6,5 @@ rem       stratum-asia.rplant.xyz /Singapore/
 rem       stratum-na.rplant.xyz   /Toronto/
 rem
 :start
-"%~dp0"avx512.exe -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC
+"%~dp0"avx2.exe -a minotaurx -o stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 -u LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn -p c=LTC
 goto start
